fd8d30317a315cd7ca4a140afcc6aca946fc62fb
雅史 石川 <a622740@gmail.com>
2017-01-12 15:11:04 +0900

    修正

--- rapt/built/out.js
+++ rapt/built/out.js
@@ -621,6 +621,35 @@ var keyCodeArray = [, , ,
     ,
     'META'
 ];
+// class Keyframe
+var Keyframe = (function () {
+    function Keyframe(x, y) {
+        this.angles = [];
+        this.center = new Vector(x, y);
+        // this.angles = [];
+    }
+    Keyframe.prototype.add = function () {
+        for (var i = 0; i < arguments.length; i++) {
+            this.angles.push(arguments[i] * Math.PI / 180);
+        }
+        return this;
+    };
+    Keyframe.prototype.lerpWith = function (keyframe, percent) {
+        var result = new Keyframe(lerp(this.center.x, keyframe.center.x, percent), lerp(this.center.y, keyframe.center.y, percent));
+        for (var i = 0; i < this.angles.length; i++) {
+            result.angles.push(lerp(this.angles[i], keyframe.angles[i], percent));
+        }
+        return result;
+    };
+    Keyframe.lerp = function (keyframes, percent) {
+        var lower = Math.floor(percent);
+        percent -= lower;
+        lower = lower % keyframes.length;
+        var upper = (lower + 1) % keyframes.length;
+        return keyframes[lower].lerpWith(keyframes[upper], percent);
+    };
+    return Keyframe;
+}());
 function setLocalStorage(name, value) {
     // attempt to use localStorage first
     if (typeof localStorage != 'undefined') {
---
+++ rapt/src/util/keyframe.ts
@@ -0,0 +1,35 @@
+// class Keyframe
+class Keyframe {
+    center;
+    angles = [];
+    constructor(x, y) {
+        this.center = new Vector(x, y);
+        // this.angles = [];
+    }
+
+    add(/* one or more angles */) {
+        for (var i = 0; i < arguments.length; i++) {
+            this.angles.push(arguments[i] * Math.PI / 180);
+        }
+        return this;
+    }
+
+    lerpWith(keyframe, percent) {
+        var result = new Keyframe(
+            lerp(this.center.x, keyframe.center.x, percent),
+            lerp(this.center.y, keyframe.center.y, percent)
+        );
+        for (var i = 0; i < this.angles.length; i++) {
+            result.angles.push(lerp(this.angles[i], keyframe.angles[i], percent));
+        }
+        return result;
+    }
+
+    static lerp(keyframes, percent) {
+        var lower = Math.floor(percent);
+        percent -= lower;
+        lower = lower % keyframes.length;
+        var upper = (lower + 1) % keyframes.length;
+        return keyframes[lower].lerpWith(keyframes[upper], percent);
+    }
+}

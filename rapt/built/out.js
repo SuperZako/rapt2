e5f805a0c151fe6ea1e43a86d9935027f07e851b
雅史 石川 <a622740@gmail.com>
2017-01-12 13:48:18 +0900

    修正

--- rapt/built/out.js
+++ rapt/built/out.js
@@ -65,6 +65,56 @@ var AABB = (function () {
     };
     return AABB;
 }());
+// class Circle extends Shape
+var Circle = (function () {
+    function Circle(center, radius) {
+        this.center = center;
+        this.radius = radius;
+        // this.center = center;
+        // this.radius = radius;
+    }
+    Circle.prototype.copy = function () {
+        return new Circle(this.center, this.radius);
+    };
+    Circle.prototype.getType = function () {
+        return SHAPE_CIRCLE;
+    };
+    Circle.prototype.getAabb = function () {
+        var radiusVector = new Vector(this.radius, this.radius);
+        return new AABB(this.center.sub(radiusVector), this.center.add(radiusVector));
+    };
+    Circle.prototype.getCenter = function () {
+        return this.center;
+    };
+    Circle.prototype.moveBy = function (delta) {
+        this.center = this.center.add(delta);
+    };
+    Circle.prototype.moveTo = function (destination) {
+        this.center = destination;
+    };
+    Circle.prototype.offsetBy = function (offset) {
+        return new Circle(this.center.add(offset), this.radius);
+    };
+    Circle.prototype.draw = function (c) {
+        c.strokeStyle = 'black';
+        c.beginPath();
+        c.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2, false);
+        c.stroke();
+    };
+    return Circle;
+}());
+// class Contact
+var Contact = (function () {
+    function Contact(contactPoint, normal, proportionOfDelta) {
+        this.contactPoint = contactPoint;
+        this.normal = normal;
+        this.proportionOfDelta = proportionOfDelta;
+        // this.proportionOfDelta = proportionOfDelta;
+        // this.contactPoint = contactPoint;
+        // this.normal = normal;
+    }
+    return Contact;
+}());
 /**
   *  For the polygon class, the segments and the bounding box are all relative to the center of the polygon.
   *  That is, when the polygon moves, the center is the only thing that changes.  This is to prevent
---
+++ rapt/src/collisions/contact.ts
@@ -0,0 +1,8 @@
+// class Contact
+class Contact {
+    constructor(public contactPoint, public normal, public proportionOfDelta) {
+        // this.proportionOfDelta = proportionOfDelta;
+        // this.contactPoint = contactPoint;
+        // this.normal = normal;
+    }
+}
